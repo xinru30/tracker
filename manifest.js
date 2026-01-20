bda314f135e7b3a0491698cefa57b2ac4a9d90a8
xinru30 <101734447+xinru30@users.noreply.github.com>
2026-01-20 12:39:01 +0800

    Add files via upload

---
+++ manifest.js
@@ -0,0 +1,15 @@
+{
+  "name": "同人新刊進度 Tracker",
+  "short_name": "新刊進度",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#4a5568",
+  "icons": [
+    {
+      "src": "https://cdn-icons-png.flaticon.com/512/2292/2292364.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
